7d59147603aecaacae206c824b051792a4cab09d
ASUS <Yehor Kholodiuk>
2022-11-17 13:12:21 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import db from './connectDB'
-import {collection, getDocs} from 'firebase/firestore'
+import {collection, getDocs, query, onSnapshot} from 'firebase/firestore'
 import './App.css';
 import {useEffect, useState} from "react";
 
 function App() {
     const [tasks, setTasks] = useState([])
+   // useEffect(() => {
+           // const getTasks = async () => {
+               // const tasksCol = collection(db, 'tasks');
+               // const tasksSnapshot = await getDocs(tasksCol);
+               // const tasksList = tasksSnapshot.docs.map(doc => doc.data());
+               // console.log(tasksList)
+               // setTasks(tasksList)
+           // }
+          //  getTasks()
+      //  }
+   // )
+
     useEffect(() => {
-            const getTasks = async () => {
-                const tasksCol = collection(db, 'tasks');
-                const tasksSnapshot = await getDocs(tasksCol);
-                const tasksList = tasksSnapshot.docs.map(doc => doc.data());
-                console.log(tasksList)
-                setTasks(tasksList)
-            }
-            getTasks()
-        }
-    )
+        const taskColRef = query (collection(db,'tasks'));
+        onSnapshot ( taskColRef, (snapshot) => {
+            setTasks (snapshot.docs.map(doc => ({
+                id:doc.id,
+                ...doc.data()
+            })))
+
+        });
+    }, [])
+    console.log(tasks)
     return (
         <div className="App">
             <ul>
